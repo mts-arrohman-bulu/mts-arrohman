73167e27a0f847393bf785b2a737d3b7bc639dbb
mts-arrohman-bulu <mtsarrohman01bulu@gmail.com>
2026-02-21 14:26:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const form = document.getElementById("ppdbForm");
+  const scriptURL = "PASTE_URL_WEBAPP_BARU_DI_SINI";
+  const form = document.getElementById("ppdbForm");
 
-    if (form) {  // <- ini penting supaya tidak error di halaman lain
+  if (form) {
 
-        const scriptURL = "https://script.google.com/macros/s/AKfycbyMuUXlEudKs4iMeQQgveOCRTXpHEVdLTzOo_-_4B1lcsQWOKojRO95uxr-n3ey3HYt/exec";
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
 
-        form.addEventListener("submit", function (e) {
-            e.preventDefault();
+      fetch(scriptURL, {
+        method: "POST",
+        mode: "no-cors",
+        body: new URLSearchParams({
+          nama: document.getElementById("nama").value,
+          asal: document.getElementById("asal").value,
+          jk: document.getElementById("jk").value,
+          ortu: document.getElementById("ortu").value,
+          hp: document.getElementById("hp").value,
+          alamat: document.getElementById("alamat").value
+        })
+      });
 
-            fetch(scriptURL, {
-    method: 'POST',
-    mode: 'no-cors',   // ← TAMBAHKAN INI
-    body: new URLSearchParams({
-        nama: document.getElementById("nama").value,
-        asal: document.getElementById("asal").value,
-        jk: document.getElementById("jk").value,
-        ortu: document.getElementById("ortu").value,
-        hp: document.getElementById("hp").value,
-        alamat: document.getElementById("alamat").value
-    })
-})
-.then(() => {
-    document.getElementById("notif").innerHTML = "Pendaftaran berhasil!";
-    document.getElementById("ppdbForm").reset();
-})
-.catch(() => {
-    document.getElementById("notif").innerHTML = "Terjadi kesalahan.";
+      document.getElementById("notif").innerHTML = "Pendaftaran berhasil!";
+      form.reset();
+    });
+
+  }
+
 });
-
-
-
-
-
