0211f43138425a35996a18bb3b5a420427bff834
mts-arrohman-bulu <mtsarrohman01bulu@gmail.com>
2026-02-24 14:01:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbx9h0DYD_f7KOeaVCD5nKdecQdMwWdGfMfJsZNrOtsyA4wZO23bkSCFAG3qIYJ-19GCjA/exec";
 
-const form = document.getElementById("ppdbForm");
+document.addEventListener("DOMContentLoaded", function() {
 
-form.addEventListener("submit", function(e){
-  e.preventDefault();
+  const form = document.getElementById("ppdbForm");
 
-  fetch(scriptURL, {
-    method: 'POST',
-    body: new FormData(form)
-  })
-  .then(response => response.text())
-  .then(nomor => {
+  if(form){
+    form.addEventListener("submit", function(e){
+      e.preventDefault();
 
-    const query = new URLSearchParams(new FormData(form)).toString();
+      fetch(scriptURL, {
+        method: 'POST',
+        body: new FormData(form)
+      })
+      .then(response => response.text())
+      .then(nomor => {
 
-    window.location.href = "cetak.html?no=" + nomor + "&" + query;
+        const query = new URLSearchParams(new FormData(form)).toString();
+        window.location.href = "cetak.html?no=" + nomor + "&" + query;
 
-  })
-  .catch(error => {
-    alert("Terjadi kesalahan.");
-  });
+      })
+      .catch(error => {
+        alert("Terjadi kesalahan.");
+      });
+    });
+  }
+
 });
